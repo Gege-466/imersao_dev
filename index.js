1b8f370ee23769781868bf41f276ae884c8f28e6
Geovanna Lima <lsgeovanna072@gmail.com>
2022-04-13 17:38:19 -0300

    fixei a nota para apenas uma casa decimal

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 function NotaFinal() {
-let notaPrimeiroBimestre = (document.getElementById("nota1").value) 
-let notaSegundoBimestre = (document.getElementById("nota2").value) 
-let notaTerceiroBimestre = (document.getElementById("nota3").value) 
-let notaQuartoBimestre = (document.getElementById("nota4").value) 
+    let notaPrimeiroBimestre = (document.getElementById("nota1").value) 
+    let notaSegundoBimestre = (document.getElementById("nota2").value) 
+    let notaTerceiroBimestre = (document.getElementById("nota3").value) 
+    let notaQuartoBimestre = (document.getElementById("nota4").value) 
 
-let n1 = parseInt(notaPrimeiroBimestre)
-let n2 = parseInt(notaSegundoBimestre)
-let n3 = parseInt(notaTerceiroBimestre)
-let n4 = parseInt(notaQuartoBimestre)
+    let n1 = parseInt(notaPrimeiroBimestre)
+    let n2 = parseInt(notaSegundoBimestre)
+    let n3 = parseInt(notaTerceiroBimestre)
+    let n4 = parseInt(notaQuartoBimestre)
 
-let nota = (n1 + n2 + n3 + n4) / 4 
+    let nota = ((n1 + n2 + n3 + n4) / 4).toFixed(1) 
 
 
-    let elemento = document.getElementById("resultado")
-    if (nota >= 10) {
-        elemento.innerText = "Aprovado"
-    } else if (nota >= 5) {
-        elemento.innerText = "Recuperação"
-    } else {
-        elemento.innerText = "Reprovado"
+        let elemento = document.getElementById("resultado")
+        if (nota >= 10) {
+            elemento.innerText = "Aprovado"
+        } else if (nota >= 5) {
+            elemento.innerText = "Recuperação"
+        } else {
+            elemento.innerText = "Reprovado"
+        }
     }
-}
  
