2fc1f3320b2c52b7e1758ba79b3e7f53f024e109
Geovanna Lima <lsgeovanna072@gmail.com>
2022-04-13 17:22:09 -0300

    consertei o erro da imagem de fundo e o erro do calculo de notas

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
-let notaPrimeiroBimestre = getElementById("n1").value
-let notaSegundoBimestre = getElementById("n2").value
-let notaTerceiroBimestre = getElementById("n3").value
-let notaQuartoBimestre = getElementById("n4").value
-
-let nota = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) 
-
 function NotaFinal() {
+let notaPrimeiroBimestre = (document.getElementById("nota1").value) 
+let notaSegundoBimestre = (document.getElementById("nota2").value) 
+let notaTerceiroBimestre = (document.getElementById("nota3").value) 
+let notaQuartoBimestre = (document.getElementById("nota4").value) 
+
+let n1 = parseInt(notaPrimeiroBimestre)
+let n2 = parseInt(notaSegundoBimestre)
+let n3 = parseInt(notaTerceiroBimestre)
+let n4 = parseInt(notaQuartoBimestre)
+
+let nota = (n1 + n2 + n3 + n4) / 4 
+
+
     let elemento = document.getElementById("resultado")
-    if (nota >= 20) {
+    if (nota >= 10) {
         elemento.innerText = "Aprovado"
-    } else if (nota >= 15) {
+    } else if (nota >= 5) {
         elemento.innerText = "Recuperação"
     } else {
         elemento.innerText = "Reprovado"
     }
 }
+ 
